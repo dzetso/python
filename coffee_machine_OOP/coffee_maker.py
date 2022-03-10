a09e995939caf81358e13e63e55e7824447e33b5
dzetso <dzetso@gmail.com>
2022-03-10 23:16:11 +0500

    update

--- coffee_machine_OOP/coffee_maker.py
+++ coffee_machine_OOP/coffee_maker.py
@@ -8,7 +8,7 @@ class CoffeeMaker:
         }
 
     def report(self):
-        """Prints a report of all resources."""
+        """Prints a report of all resources. """
         print(f"Water: {self.resources['water']}ml")
         print(f"Milk: {self.resources['milk']}ml")
         print(f"Coffee: {self.resources['coffee']}g")
