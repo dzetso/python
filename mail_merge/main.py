5df7c9b44cd7b4651bdd22f4c283eb4534224d32
dzetso <dzetso@gmail.com>
2022-03-26 23:07:51 +0500

    update

--- mail_merge/main.py
+++ mail_merge/main.py
@@ -5,6 +5,7 @@ names_list = []
 for number in range(0, len(raw_names_list)):
     names_list.append(raw_names_list[number].replace("\n", ""))
 
+# Writing letters for every name in the names list
 for name in names_list:
     with open("mail_merge/Input/Letters/starting_letter.txt") as example:
         example_text = example.read()
